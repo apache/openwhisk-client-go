0c4e5cf606d34bfb4cd1c21d3181b4d8294344ce
Theodore Wilson <theodore.wilson@us.ibm.com>
2016-01-19 20:52:26 -0800

    extracted from whisk-cli; changed package to 'whisk'

---
+++ namespace.go
@@ -0,0 +1,29 @@
+package whisk
+
+import "net/http"
+
+type Namespace string
+
+type NamespaceService struct {
+	client *Client
+}
+
+func (s *NamespaceService) List() ([]Namespace, *http.Response, error) {
+	// make a request to c.BaseURL
+
+	url := s.client.BaseURL.String() // "whisk.stage1.ng.bluemix.net"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	s.client.addAuthHeader(req)
+
+	var namespaces []Namespace
+	resp, err := s.client.Do(req, &namespaces)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return namespaces, resp, nil
+}
