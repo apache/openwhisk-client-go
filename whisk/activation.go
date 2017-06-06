apache/openwhisk

Return Non-zero Exit Code when Action Exceeds Time Limit

tools/cli/go-whisk/whisk/activation.go
@@ -95,7 +95,7 @@ func (s *ActivationService) List(options *ActivationListOptions) ([]Activation, 
     Debug(DbgInfo, "Sending HTTP request - URL '%s'; req %#v\n", req.URL.String(), req)
 
     var activations []Activation
-    resp, err := s.client.Do(req, &activations)
+    resp, err := s.client.Do(req, &activations, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -126,7 +126,7 @@ func (s *ActivationService) Get(activationID string) (*Activation, *http.Respons
     Debug(DbgInfo, "Sending HTTP request - URL '%s'; req %#v\n", req.URL.String(), req)
 
     a := new(Activation)
-    resp, err := s.client.Do(req, &a)
+    resp, err := s.client.Do(req, &a, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -155,7 +155,7 @@ func (s *ActivationService) Logs(activationID string) (*Activation, *http.Respon
     Debug(DbgInfo, "Sending HTTP request - URL '%s'; req %#v\n", req.URL.String(), req)
 
     activation := new(Activation)
-    resp, err := s.client.Do(req, &activation)
+    resp, err := s.client.Do(req, &activation, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -184,7 +184,7 @@ func (s *ActivationService) Result(activationID string) (*Response, *http.Respon
     Debug(DbgInfo, "Sending HTTP request - URL '%s'; req %#v\n", req.URL.String(), req)
 
     r := new(Response)
-    resp, err := s.client.Do(req, &r)
+    resp, err := s.client.Do(req, &r, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, resp, err

tools/cli/go-whisk/whisk/api.go
@@ -152,7 +152,7 @@ func (s *ApiService) List(apiListOptions *ApiListOptions) (*RetApiArray, *http.R
     }
 
     apiArray := new(RetApiArray)
-    resp, err := s.client.Do(req, &apiArray)
+    resp, err := s.client.Do(req, &apiArray, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -178,7 +178,7 @@ func (s *ApiService) Insert(api *SendApi, overwrite bool) (*RetApi, *http.Respon
     }
 
     retApi := new(RetApi)
-    resp, err := s.client.Do(req, &retApi)
+    resp, err := s.client.Do(req, &retApi, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -213,7 +213,7 @@ func (s *ApiService) Get(api *Api, options *ApiListOptions) (*RetApiArray, *http
     }
 
     retApi := new(RetApiArray)
-    resp, err := s.client.Do(req, &retApi)
+    resp, err := s.client.Do(req, &retApi, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -248,7 +248,7 @@ func (s *ApiService) Delete(api *Api, options *ApiOptions) (*http.Response, erro
     }
 
     retApi := new(RetApi)
-    resp, err := s.client.Do(req, &retApi)
+    resp, err := s.client.Do(req, &retApi, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return resp, err

tools/cli/go-whisk/whisk/info.go
@@ -58,7 +58,7 @@ func (s *InfoService) Get() (*Info, *http.Response, error) {
 
     Debug(DbgInfo, "Sending HTTP URL '%s'; req %#v\n", req.URL.String(), req)
     info := new(Info)
-    resp, err := s.client.Do(req, &info)
+    resp, err := s.client.Do(req, &info, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, nil, err

tools/cli/go-whisk/whisk/namespace.go
@@ -55,7 +55,7 @@ func (s *NamespaceService) List() ([]Namespace, *http.Response, error) {
     }
 
     var namespaceNames []string
-    resp, err := s.client.Do(req, &namespaceNames)
+    resp, err := s.client.Do(req, &namespaceNames, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -92,7 +92,7 @@ func (s *NamespaceService) Get(namespace string) (*Namespace, *http.Response, er
         return resNamespace, nil, werr
     }
 
-    resp, err := s.client.Do(req, &resNamespace.Contents)
+    resp, err := s.client.Do(req, &resNamespace.Contents, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return resNamespace, resp, err

tools/cli/go-whisk/whisk/package.go
@@ -103,7 +103,7 @@ func (s *PackageService) List(options *PackageListOptions) ([]Package, *http.Res
     }
 
     var packages []Package
-    resp, err := s.client.Do(req, &packages)
+    resp, err := s.client.Do(req, &packages, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -129,7 +129,7 @@ func (s *PackageService) Get(packageName string) (*Package, *http.Response, erro
     }
 
     p := new(Package)
-    resp, err := s.client.Do(req, &p)
+    resp, err := s.client.Do(req, &p, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -155,7 +155,7 @@ func (s *PackageService) Insert(x_package PackageInterface, overwrite bool) (*Pa
     }
 
     p := new(Package)
-    resp, err := s.client.Do(req, &p)
+    resp, err := s.client.Do(req, &p, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -179,7 +179,7 @@ func (s *PackageService) Delete(packageName string) (*http.Response, error) {
         return nil, werr
     }
 
-    resp, err := s.client.Do(req, nil)
+    resp, err := s.client.Do(req, nil, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return resp, err
@@ -201,7 +201,7 @@ func (s *PackageService) Refresh() (*BindingUpdates, *http.Response, error) {
     }
 
     updates := &BindingUpdates{}
-    resp, err := s.client.Do(req, updates)
+    resp, err := s.client.Do(req, updates, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err

tools/cli/go-whisk/whisk/rule.go
@@ -67,7 +67,7 @@ func (s *RuleService) List(options *RuleListOptions) ([]Rule, *http.Response, er
     }
 
     var rules []Rule
-    resp, err := s.client.Do(req, &rules)
+    resp, err := s.client.Do(req, &rules, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -92,7 +92,7 @@ func (s *RuleService) Insert(rule *Rule, overwrite bool) (*Rule, *http.Response,
     }
 
     r := new(Rule)
-    resp, err := s.client.Do(req, &r)
+    resp, err := s.client.Do(req, &r, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -117,7 +117,7 @@ func (s *RuleService) Get(ruleName string) (*Rule, *http.Response, error) {
     }
 
     r := new(Rule)
-    resp, err := s.client.Do(req, &r)
+    resp, err := s.client.Do(req, &r, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -141,7 +141,7 @@ func (s *RuleService) Delete(ruleName string) (*http.Response, error) {
         return nil, werr
     }
 
-    resp, err := s.client.Do(req, nil)
+    resp, err := s.client.Do(req, nil, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return resp, err
@@ -176,7 +176,7 @@ func (s *RuleService) SetState(ruleName string, state string) (*Rule, *http.Resp
     }
 
     r := new(Rule)
-    resp, err := s.client.Do(req, &r)
+    resp, err := s.client.Do(req, &r, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err

tools/cli/go-whisk/whisk/trigger.go
@@ -67,14 +67,13 @@ func (s *TriggerService) List(options *TriggerListOptions) ([]Trigger, *http.Res
     }
 
     var triggers []Trigger
-    resp, err := s.client.Do(req, &triggers)
+    resp, err := s.client.Do(req, &triggers, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err
     }
 
     return triggers, resp, nil
-
 }
 
 func (s *TriggerService) Insert(trigger *Trigger, overwrite bool) (*Trigger, *http.Response, error) {
@@ -102,7 +101,7 @@ func (s *TriggerService) Insert(trigger *Trigger, overwrite bool) (*Trigger, *ht
     }
 
     t := new(Trigger)
-    resp, err := s.client.Do(req, &t)
+    resp, err := s.client.Do(req, &t, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -128,7 +127,7 @@ func (s *TriggerService) Get(triggerName string) (*Trigger, *http.Response, erro
     }
 
     t := new(Trigger)
-    resp, err := s.client.Do(req, &t)
+    resp, err := s.client.Do(req, &t, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -154,7 +153,7 @@ func (s *TriggerService) Delete(triggerName string) (*Trigger, *http.Response, e
     }
 
     t := new(Trigger)
-    resp, err := s.client.Do(req, &t)
+    resp, err := s.client.Do(req, &t, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -179,7 +178,7 @@ func (s *TriggerService) Fire(triggerName string, payload interface{}) (*Trigger
     }
 
     t := new(Trigger)
-    resp, err := s.client.Do(req, &t)
+    resp, err := s.client.Do(req, &t, DoNotProcessTimeOut)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err

apache/openwhisk-client-go

whisk/activation.go
@@ -95,7 +95,7 @@ func (s *ActivationService) List(options *ActivationListOptions) ([]Activation, 
     Debug(DbgInfo, "Sending HTTP request - URL '%s'; req %#v\n", req.URL.String(), req)
 
     var activations []Activation
-    resp, err := s.client.Do(req, &activations)
+    resp, err := s.client.Do(req, &activations, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -126,7 +126,7 @@ func (s *ActivationService) Get(activationID string) (*Activation, *http.Respons
     Debug(DbgInfo, "Sending HTTP request - URL '%s'; req %#v\n", req.URL.String(), req)
 
     a := new(Activation)
-    resp, err := s.client.Do(req, &a)
+    resp, err := s.client.Do(req, &a, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -155,7 +155,7 @@ func (s *ActivationService) Logs(activationID string) (*Activation, *http.Respon
     Debug(DbgInfo, "Sending HTTP request - URL '%s'; req %#v\n", req.URL.String(), req)
 
     activation := new(Activation)
-    resp, err := s.client.Do(req, &activation)
+    resp, err := s.client.Do(req, &activation, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -184,7 +184,7 @@ func (s *ActivationService) Result(activationID string) (*Response, *http.Respon
     Debug(DbgInfo, "Sending HTTP request - URL '%s'; req %#v\n", req.URL.String(), req)
 
     r := new(Response)
-    resp, err := s.client.Do(req, &r)
+    resp, err := s.client.Do(req, &r, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, resp, err

whisk/api.go
@@ -152,7 +152,7 @@ func (s *ApiService) List(apiListOptions *ApiListOptions) (*RetApiArray, *http.R
     }
 
     apiArray := new(RetApiArray)
-    resp, err := s.client.Do(req, &apiArray)
+    resp, err := s.client.Do(req, &apiArray, ExitWithErrorOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -178,7 +178,7 @@ func (s *ApiService) Insert(api *SendApi, overwrite bool) (*RetApi, *http.Respon
     }
 
     retApi := new(RetApi)
-    resp, err := s.client.Do(req, &retApi)
+    resp, err := s.client.Do(req, &retApi, ExitWithErrorOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -213,7 +213,7 @@ func (s *ApiService) Get(api *Api, options *ApiListOptions) (*RetApiArray, *http
     }
 
     retApi := new(RetApiArray)
-    resp, err := s.client.Do(req, &retApi)
+    resp, err := s.client.Do(req, &retApi, ExitWithErrorOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -248,7 +248,7 @@ func (s *ApiService) Delete(api *Api, options *ApiOptions) (*http.Response, erro
     }
 
     retApi := new(RetApi)
-    resp, err := s.client.Do(req, &retApi)
+    resp, err := s.client.Do(req, &retApi, ExitWithErrorOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return resp, err

whisk/info.go
@@ -58,7 +58,7 @@ func (s *InfoService) Get() (*Info, *http.Response, error) {
 
     Debug(DbgInfo, "Sending HTTP URL '%s'; req %#v\n", req.URL.String(), req)
     info := new(Info)
-    resp, err := s.client.Do(req, &info)
+    resp, err := s.client.Do(req, &info, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, nil, err

whisk/namespace.go
@@ -55,7 +55,7 @@ func (s *NamespaceService) List() ([]Namespace, *http.Response, error) {
     }
 
     var namespaceNames []string
-    resp, err := s.client.Do(req, &namespaceNames)
+    resp, err := s.client.Do(req, &namespaceNames, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -92,7 +92,7 @@ func (s *NamespaceService) Get(namespace string) (*Namespace, *http.Response, er
         return resNamespace, nil, werr
     }
 
-    resp, err := s.client.Do(req, &resNamespace.Contents)
+    resp, err := s.client.Do(req, &resNamespace.Contents, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return resNamespace, resp, err

whisk/package.go
@@ -103,7 +103,7 @@ func (s *PackageService) List(options *PackageListOptions) ([]Package, *http.Res
     }
 
     var packages []Package
-    resp, err := s.client.Do(req, &packages)
+    resp, err := s.client.Do(req, &packages, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -129,7 +129,7 @@ func (s *PackageService) Get(packageName string) (*Package, *http.Response, erro
     }
 
     p := new(Package)
-    resp, err := s.client.Do(req, &p)
+    resp, err := s.client.Do(req, &p, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -155,7 +155,7 @@ func (s *PackageService) Insert(x_package PackageInterface, overwrite bool) (*Pa
     }
 
     p := new(Package)
-    resp, err := s.client.Do(req, &p)
+    resp, err := s.client.Do(req, &p, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -179,7 +179,7 @@ func (s *PackageService) Delete(packageName string) (*http.Response, error) {
         return nil, werr
     }
 
-    resp, err := s.client.Do(req, nil)
+    resp, err := s.client.Do(req, nil, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return resp, err
@@ -201,7 +201,7 @@ func (s *PackageService) Refresh() (*BindingUpdates, *http.Response, error) {
     }
 
     updates := &BindingUpdates{}
-    resp, err := s.client.Do(req, updates)
+    resp, err := s.client.Do(req, updates, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err

whisk/rule.go
@@ -67,7 +67,7 @@ func (s *RuleService) List(options *RuleListOptions) ([]Rule, *http.Response, er
     }
 
     var rules []Rule
-    resp, err := s.client.Do(req, &rules)
+    resp, err := s.client.Do(req, &rules, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -92,7 +92,7 @@ func (s *RuleService) Insert(rule *Rule, overwrite bool) (*Rule, *http.Response,
     }
 
     r := new(Rule)
-    resp, err := s.client.Do(req, &r)
+    resp, err := s.client.Do(req, &r, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -117,7 +117,7 @@ func (s *RuleService) Get(ruleName string) (*Rule, *http.Response, error) {
     }
 
     r := new(Rule)
-    resp, err := s.client.Do(req, &r)
+    resp, err := s.client.Do(req, &r, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -141,7 +141,7 @@ func (s *RuleService) Delete(ruleName string) (*http.Response, error) {
         return nil, werr
     }
 
-    resp, err := s.client.Do(req, nil)
+    resp, err := s.client.Do(req, nil, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return resp, err
@@ -176,7 +176,7 @@ func (s *RuleService) SetState(ruleName string, state string) (*Rule, *http.Resp
     }
 
     r := new(Rule)
-    resp, err := s.client.Do(req, &r)
+    resp, err := s.client.Do(req, &r, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err

whisk/trigger.go
@@ -67,14 +67,13 @@ func (s *TriggerService) List(options *TriggerListOptions) ([]Trigger, *http.Res
     }
 
     var triggers []Trigger
-    resp, err := s.client.Do(req, &triggers)
+    resp, err := s.client.Do(req, &triggers, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err
     }
 
     return triggers, resp, nil
-
 }
 
 func (s *TriggerService) Insert(trigger *Trigger, overwrite bool) (*Trigger, *http.Response, error) {
@@ -102,7 +101,7 @@ func (s *TriggerService) Insert(trigger *Trigger, overwrite bool) (*Trigger, *ht
     }
 
     t := new(Trigger)
-    resp, err := s.client.Do(req, &t)
+    resp, err := s.client.Do(req, &t, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -128,7 +127,7 @@ func (s *TriggerService) Get(triggerName string) (*Trigger, *http.Response, erro
     }
 
     t := new(Trigger)
-    resp, err := s.client.Do(req, &t)
+    resp, err := s.client.Do(req, &t, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -154,7 +153,7 @@ func (s *TriggerService) Delete(triggerName string) (*Trigger, *http.Response, e
     }
 
     t := new(Trigger)
-    resp, err := s.client.Do(req, &t)
+    resp, err := s.client.Do(req, &t, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err
@@ -179,7 +178,7 @@ func (s *TriggerService) Fire(triggerName string, payload interface{}) (*Trigger
     }
 
     t := new(Trigger)
-    resp, err := s.client.Do(req, &t)
+    resp, err := s.client.Do(req, &t, ExitWithSuccessOnTimeout)
     if err != nil {
         Debug(DbgError, "s.client.Do() error - HTTP req %s; error: '%s'\n", req.URL.String(), err)
         return nil, resp, err