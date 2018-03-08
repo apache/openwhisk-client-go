012fd0fb022fd542fdd7606567d0c9b38ca56bd8
James Dubee <jwdubee@us.ibm.com>
2018-03-07 23:13:59 -0500

    Additional HTTP Headers (#67)
    * Allow Additional HTTP Headers
    
    * Add AdditionalHeader Tests
    
    * Use http.Header as AdditionalHeader type

--- whisk/client_test.go
+++ whisk/client_test.go
@@ -140,3 +140,31 @@ func TestProxyHost(t *testing.T) {
 		assert.Contains(t, err.Error(), proxyhost, "Setting HTTPS_PROXY to '"+proxyhost+"' did not cause the CLI to use that proxy URL.")
 	}
 }
+
+func TestAdditionalHeaders(t *testing.T) {
+	config := GetValidConfigTest()
+	config.AdditionalHeaders = make(map[string][]string)
+	config.AdditionalHeaders.Add("Key1", "Value1")
+	config.AdditionalHeaders.Add("Key2", "Value2")
+
+	client, _ := NewClient(nil, config)
+	assert.NotNil(t, client)
+
+	newRequest, newRequestErr := client.NewRequest("GET", config.BaseURL.String(), nil, false)
+	assert.Nil(t, newRequestErr, "NewRequest for proxy test failed.")
+	if newRequestErr != nil {
+		fmt.Printf("NewRequest() error: %s\n", newRequestErr.Error())
+	}
+
+	assert.Equal(t, "Value1", newRequest.Header.Get("Key1"))
+	assert.Equal(t, "Value2", newRequest.Header.Get("Key2"))
+
+	newRequestUrl, newRequestUrlErr := client.NewRequestUrl("GET", config.BaseURL, nil, false, false, "", false)
+	assert.Nil(t, newRequestUrlErr, "NewRequest for proxy test failed.")
+	if newRequestUrlErr != nil {
+		fmt.Printf("NewRequest() error: %s\n", newRequestUrlErr.Error())
+	}
+
+	assert.Equal(t, "Value1", newRequestUrl.Header.Get("Key1"))
+	assert.Equal(t, "Value2", newRequestUrl.Header.Get("Key2"))
+}
